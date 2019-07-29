91dec25fd2aa949f19f70ce935f9f5d1ffec1cc4
sriramgullipalli <srirammanikantagullipalli@gmail.com>
2019-07-29 10:23:09 +0530

    SLL

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    class Program
+
+    class LinkedList
     {
-        static void Main(string[] args)
+        node head;
+        public class node
         {
+            public int data;
+            public node next;
+
+            public node(int d)
+            {
+                data = d;
+                next = null;
+            }
+
         }
+
+
+        public static void Main(string[] args)
+        {
+            LinkedList ll = new LinkedList();
+            ll.head = new node(1);
+            node first = new node(2);
+            node secound = new node(3);
+
+            ll.head.next = first;
+            first.next = secound;
+
+            while (ll.head != null)
+            {
+                Console.WriteLine(ll.head.data + " ");
+                ll.head = ll.head.next;
+            }
+
+        }
+
     }
 }
+
